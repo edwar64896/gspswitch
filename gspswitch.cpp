4b0a4ebb5f8904e0454841f1ba0e551ed297ddb8
edwar64896 <mark.p.edwards@gmail.com>
2020-06-15 11:28:48 +1000

    initial

---
+++ gspswitch.cpp
@@ -0,0 +1,225 @@
+#include "Arduino.h"
+#include "gspswitch.h"
+
+gspGrouped* gspSwitch::firstInstance=nullptr;
+
+//default constructor.
+//nothing will happen with this.
+gspSwitch::gspSwitch()
+:gspGrouped() {
+	_callback_off = nullptr;
+	_callback_on = nullptr;
+	_strOn = nullptr;
+	_strOff = nullptr;
+	_switchMode = gspSwitch_MODE_UNCONFIGURED;
+}
+
+//2-way toggle switch constructor
+//with callbacks
+gspSwitch::gspSwitch(uint8_t pin, void (*cb_off)(), void (*cb_on)())
+:gspGrouped() {
+	_callback_off = cb_off;
+	_callback_on = cb_on;
+	_pin = pin;
+	pinMode(_pin, INPUT_PULLUP);
+	_switchMode = gspSwitch_MODE_SWITCH_CB;
+}
+
+//2-way toggle switch constructor with strings
+gspSwitch::gspSwitch(uint8_t pin, const char * strOff, const char * strOn)
+:gspGrouped() {
+	_strOff = strOff;
+	_strOn = strOn;
+	_pin = pin;
+	pinMode(_pin, INPUT_PULLUP);
+	_switchMode = gspSwitch_MODE_SWITCH_STR;
+}
+
+//pushbutton constructor
+//with callback
+gspSwitch::gspSwitch(uint8_t pin, void (*cb_on)(), uint8_t mode)
+:gspGrouped() {
+	_callback_on = cb_on;
+	_callback_off = nullptr;
+	_pin = pin;
+	pinMode(_pin, INPUT_PULLUP);
+	switch (mode) {
+		case 1:
+			_switchMode = gspSwitch_MODE_PUSHBUTTON_PUSH_CB;
+			break;
+		case 2:
+			_switchMode = gspSwitch_MODE_PUSHBUTTON_CONTINUOUS_CB;
+			break;
+		case 0:
+			_switchMode = gspSwitch_MODE_PUSHBUTTON_RELEASE_CB;
+			break;
+		default:
+			_switchMode = gspSwitch_MODE_UNCONFIGURED;
+			break;
+	}
+}
+
+//pushbutton constructor with string
+gspSwitch::gspSwitch(uint8_t pin, const char * strOn, uint8_t mode)
+:gspGrouped() {
+	_strOn = strOn;
+	_strOff = nullptr;
+	_pin = pin;
+	pinMode(_pin, INPUT_PULLUP);
+	switch (mode) {
+		case 1:
+			_switchMode = gspSwitch_MODE_PUSHBUTTON_PUSH_STR;
+			break;
+		case 2:
+			_switchMode = gspSwitch_MODE_PUSHBUTTON_CONTINUOUS_STR;
+			break;
+		case 0:
+			_switchMode = gspSwitch_MODE_PUSHBUTTON_RELEASE_STR;
+			break;
+		default:
+			_switchMode = gspSwitch_MODE_UNCONFIGURED;
+			break;
+	}
+}
+
+gspSwitch::~gspSwitch() {}
+
+uint8_t gspSwitch::getState() {
+	return _switchState;
+}
+
+void gspSwitch::debugPrint(int pin) {
+#ifdef DEBUG
+	Serial.print("Switch:");
+	Serial.print(getInstanceId());
+	Serial.print(" Pin:");
+	Serial.println(pin);
+#endif
+}
+
+// instance method to check this instance (called from loop)
+bool gspSwitch::check() {
+	int drVal=digitalRead(_pin);
+	switch (_switchMode) {
+	case gspSwitch_MODE_SWITCH_CB:
+		if (drVal != _old_s1) {
+			debugPrint(_pin);	
+			if (drVal) {
+				_callback_off();
+				_switchState=gspSwitch::Off;
+			} else {
+				_callback_on();
+				_switchState=gspSwitch::On;
+			}
+			_old_s1 = drVal;
+		}
+		break;
+	case gspSwitch_MODE_SWITCH_STR:
+		if (drVal != _old_s1) {
+			debugPrint(_pin);	
+			if (drVal) {
+				Serial.println(_strOff);
+				_switchState=gspSwitch::Off;
+			} else {
+				Serial.println(_strOn);
+				_switchState=gspSwitch::On;
+			}
+			_old_s1 = drVal;
+		}
+		break;
+	case gspSwitch_MODE_PUSHBUTTON_RELEASE_CB:
+		if (!drVal) {
+			_s1 = 1;
+			_s2 = 1;
+			_switchState=gspSwitch::On;
+
+		} else {
+			_s1 = 0;
+			_switchState=gspSwitch::Off;
+		}
+
+		if (_s1 == 0 && _s2) {
+			debugPrint(_pin);	
+			_callback_on();
+			_s2 = 0;
+		}
+		break;
+	case gspSwitch_MODE_PUSHBUTTON_RELEASE_STR:
+		if (!drVal) {
+			_s1 = 1;
+			_s2 = 1;
+			_switchState=gspSwitch::On;
+		} else {
+			_s1 = 0;
+			_switchState=gspSwitch::Off;
+		}
+
+		if (_s1 == 0 && _s2) {
+			debugPrint(_pin);	
+			Serial.println(_strOn);
+			_s2 = 0;
+		}
+		break;
+	case gspSwitch_MODE_PUSHBUTTON_PUSH_CB:
+		if (!drVal) {
+			_s1count=0;
+			if (!_s1) {
+				_switchState=gspSwitch::On;
+				debugPrint(_pin);	
+				_callback_on();
+				_s1=1;
+			}
+		} else {
+			_switchState=gspSwitch::Off;
+
+			//debounce
+			if (_s1count++>1024) {
+				_s1 = 0;
+				_s1count=0;
+			}
+		}
+		break;
+	case gspSwitch_MODE_PUSHBUTTON_PUSH_STR:
+		if (!drVal) {
+			_s1count=0;
+			if (!_s1) {
+				_switchState=gspSwitch::On;
+				debugPrint(_pin);	
+				Serial.println(_strOn);
+				_s1=1;
+			}
+		} else {
+			_switchState=gspSwitch::Off;
+
+			//debounce
+			if (_s1count++>1024) {
+				_s1 = 0;
+				_s1count=0;
+			}
+		}
+		break;
+	case gspSwitch_MODE_PUSHBUTTON_CONTINUOUS_CB:
+		if (!drVal) {
+			debugPrint(_pin);	
+			_switchState=gspSwitch::On;
+			_callback_on();
+		} else {
+			_switchState=gspSwitch::Off;			
+		}
+		break;
+	case gspSwitch_MODE_PUSHBUTTON_CONTINUOUS_STR:
+		if (!drVal) {
+			debugPrint(_pin);	
+			_switchState=gspSwitch::On;
+			Serial.println(_strOn);
+		} else {
+			_switchState=gspSwitch::Off;
+		}
+		break;
+	default:
+		break;
+	}
+
+	return true;
+}
+
---
+++ gspswitch.h
@@ -0,0 +1,103 @@
+#ifndef GSP_SWITCH
+#define GSP_SWITCH
+
+#include "Arduino.h"
+#include "gspgrouped.h"
+
+#define gspSwitch_MODE_UNCONFIGURED 0
+#define gspSwitch_MODE_SWITCH_CB 1
+#define gspSwitch_MODE_SWITCH_STR 2
+#define gspSwitch_MODE_PUSHBUTTON_PUSH_CB 3
+#define gspSwitch_MODE_PUSHBUTTON_PUSH_STR 4
+#define gspSwitch_MODE_PUSHBUTTON_RELEASE_CB 5
+#define gspSwitch_MODE_PUSHBUTTON_RELEASE_STR 6
+#define gspSwitch_MODE_PUSHBUTTON_CONTINUOUS_CB 7
+#define gspSwitch_MODE_PUSHBUTTON_CONTINUOUS_STR 8
+    
+class gspSwitch:public gspGrouped
+{
+  public:
+
+    static gspSwitch * makeOne(uint8_t _pin, void (*cb1)(), void (*cb2)()) {
+        gspSwitch * instance = new gspSwitch(_pin,cb1,cb2);
+        gspGrouped::register_instance(instance);
+        return instance;
+    }
+
+    static gspSwitch * makeOne(uint8_t _pin, const char * s1, const char * s2) {
+        gspSwitch * instance = new gspSwitch(_pin,s1,s2);
+        gspGrouped::register_instance(instance);
+        return instance;
+    }
+
+    static gspSwitch * makeOne(uint8_t _pin, void (*cb1)(),uint8_t mode=0) {
+        gspSwitch * instance = new gspSwitch(_pin,cb1,mode);
+        gspGrouped::register_instance(instance);
+        return instance;
+    }
+
+    static gspSwitch * makeOne(uint8_t _pin, const char * s1,uint8_t mode=0) {
+        gspSwitch * instance = new gspSwitch(_pin,s1,mode);
+        gspGrouped::register_instance(instance);
+        return instance;
+    }
+
+    // constructors for 2-position toggle switches
+    gspSwitch(uint8_t , void (*)(), void (*)());
+    gspSwitch(uint8_t , const char *, const char *);
+
+    // constructors for momentary pushbutton switches
+    // mode = 0 (default) for "activate once on release"
+    // mode = 1 for "activate once on push"
+    // mode = 2 for "activate continuously on push"
+    gspSwitch(uint8_t , void (*)(),uint8_t mode = 0);
+    gspSwitch(uint8_t , const char *,uint8_t mode = 0);
+
+    // default constructor
+    gspSwitch();
+    ~gspSwitch();
+
+    bool check();
+
+    uint8_t getState();
+
+    enum state {
+      Off=0,
+      On
+    };
+
+
+
+    static void checkAll() {
+      gspGrouped::checkAll(gspSwitch::firstInstance);
+    }
+
+  protected:
+    void setFirstInstance(gspGrouped * fi) {gspSwitch::firstInstance = fi;}
+    gspGrouped * getFirstInstance() {return gspSwitch::firstInstance;}
+     static gspGrouped * firstInstance;
+
+  private:
+
+    void debugPrint(int pin);
+
+    void (*_callback_off)() = nullptr;
+    void (*_callback_on)() = nullptr;
+    const char* _strOff = nullptr;
+    const char* _strOn = nullptr;
+    uint8_t _pin=0;
+    int _old_s1 = -1;
+    int _s1=0;
+    int _s1count=0;
+    int _s2=0;
+    int _switchMode=gspSwitch_MODE_UNCONFIGURED;
+
+    uint8_t _switchState=gspSwitch::Off;
+
+    int switchInstance=0;
+
+    int dbug=0;
+
+};
+
+#endif
